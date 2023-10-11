01c8f1657f58a7fdc9f107dd782de7de5acc20dd
boykov <boykov.a@promail366.com>
2023-10-11 20:01:56 +0300

    Updates

---
+++ _nuxt/default.3193c1a2.js
@@ -0,0 +1,1 @@
+import{d as n,_ as o,a,b as s,D as c,o as _,E as r}from"./entry.8129944e.js";const d=n({name:"default"}),f={class:"default"};function l(e,p,u,i,m,$){const t=r;return _(),a("div",f,[s(t),c(e.$slots,"default")])}const B=o(d,[["render",l]]);export{B as default};
