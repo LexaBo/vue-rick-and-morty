56a7ee117a5111475b59cb1e5ca254c05d673ab0
boykov <boykov.a@promail366.com>
2023-10-11 19:34:42 +0300

    Updates

---
+++ assets/default.616dfcad.js
@@ -0,0 +1,1 @@
+import{b as n,_ as o,e as a,f as s,D as c,o as _,E as r}from"./entry.7e45fa08.js";const d=n({name:"default"}),f={class:"default"};function l(e,p,u,i,m,$){const t=r;return _(),a("div",f,[s(t),c(e.$slots,"default")])}const B=o(d,[["render",l]]);export{B as default};
