2c3c582523157e4a68e262f0ba2a8d89fd40cb61
boykov <boykov.a@promail366.com>
2023-10-11 20:04:20 +0300

    Updates

---
+++ assets/default.bb9acfb8.js
@@ -0,0 +1,1 @@
+import{d as n,_ as o,a,b as s,D as c,o as _,E as r}from"./entry.f008a5af.js";const d=n({name:"default"}),f={class:"default"};function l(e,p,u,i,m,$){const t=r;return _(),a("div",f,[s(t),c(e.$slots,"default")])}const B=o(d,[["render",l]]);export{B as default};
